b83ce366e24313b940dd37b6ced00d6d5d711de8
danielharada <haradad76@gmail.com>
2014-11-26 13:34:12 -0800

    Refactored mastermind.py
    Reduced size of runGame() function, split components into their own
    functions.

--- mastermind.py
+++ mastermind.py
@@ -1,43 +1,58 @@
-import mastermindUI
-import mastermindGameState
+"""mastermind.py:  Text based version of Mastermind
+See http://en.wikipedia.org/wiki/Mastermind_(board_game) for game info
+or type 'info' at any prompt during the game 
+"""
+
+import MastermindUI
+import MastermindGameState
 
 def runGame():
     play_again = True
     while play_again:
         game_over = False
 
-        this_game_UI = mastermindUI.MastermindUI()
-        this_game_UI.userDecidesIfWithSpaces()
-        this_game_UI.setColorOptions()
-        this_game_UI.generateSolution()
-        solution_pegs = this_game_UI.solution_pegs
-        
-        this_game_state = mastermindGameState.MastermindGameState(solution_pegs)
-        max_guesses = this_game_state.max_guesses
+        game_UI = MastermindUI.MastermindUI()
+        setColors(game_UI)
+        game_UI.generateSolution()
+        solution_pegs = game_UI.solution_pegs
+        game_state = MastermindGameState.MastermindGameState(solution_pegs)
+        max_guesses = game_state.max_guesses
         
         while not game_over:
-            this_game_UI.userInputsGuess()
-            guess_pegs = this_game_UI.guess_pegs
-            this_game_state.newGuess(guess_pegs)
-            this_game_state.assignResultPegs()
-            this_game_state.printResult()          
+            game_over, win = executeOneTurn(game_UI, game_state)
 
-            game_over, win = this_game_state.evaluateGameOver()
+        gameEndMessage(win, solution_pegs)
+        play_again = game_UI.userDecidesPlayAgain()
 
-            this_game_state.prepareForNewGuess() 
+def setColors(game_UI):
+    game_UI.userDecidesIfWithSpaces()
+    game_UI.setColorOptions()
 
-        if win:
-            print('Congratulations! You broke the code!')
-        else:
-            print('Sorry, you were unable to break the code in {} guesses.'.format(max_guesses))
+def executeOneTurn(game_UI, game_state):
+        game_UI.userInputsGuess()
+        guess_pegs = game_UI.guess_pegs
+        game_state.newGuess(guess_pegs)
+        game_state.assignResultPegs()
+        game_state.printResult()          
 
-        print('The solution was ', this_game_state.solution['pegs'])
+        game_over, win = game_state.evaluateGameOver()
+
+        game_state.prepareForNewGuess()
         
-        for i in range(1,this_game_state.current_guess_number):
-            print('Guess {} was'.format(i), this_game_state.history['guess{}'.format(i)],\
-                  'and the result was', this_game_state.history['result{}'.format(i)])
+        return game_over, win
 
-        play_again = this_game_UI.userDecidesPlayAgain()
+def gameEndMessage(win, solution_pegs):
+    if win:
+        print('Congratulations! You broke the code!')
+    else:
+        print('Sorry, you were unable to break the code in {} guesses.'.format(max_guesses))
+
+    print('The solution was ', solution_pegs)
+
+def printGuessHistory(game_state):
+    for i in range(1,game_state.current_guess_number):
+        print('Guess {} was'.format(i), game_state.history['guess{}'.format(i)],
+                'and the result was', game_state.history['result{}'.format(i)])
 
 if __name__ == '__main__':       
     runGame()
